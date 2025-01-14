22798c51222c1a22fd3ebe086ea2038e431e2b06
Raúl <rquispeto@unsa.edu.pe>
2025-01-13 23:14:44 -0500

    🚀feat(loader) add check for existence of config file

--- src/config/loader.rs
+++ src/config/loader.rs
@@ -6,6 +6,7 @@ use super::Config;
 pub fn load_config(path: &Path) -> Result<Config> {
     info!("Loading configuration...");
     info!("Path: {}", path.display());
+    info!("Exists: {}", path.exists());
     if !path.exists() {
         info!("No config file found at {}, using default configuration", path.display());
         return Ok(Config::default());
